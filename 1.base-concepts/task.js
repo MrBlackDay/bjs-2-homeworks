e510ef6d00c7c05bc7441cac4333dc141d07e4c6
Dima Lis <iselis777888999@gmail.com>
2024-01-24 22:25:11 +0300

    Первое задание дополнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -9,18 +9,21 @@ function solveEquation(a, b, c) {
 	} else if (d > 0) {
 		arr.push((-b + Math.sqrt(d)) / (2 * a));
 		arr.push((-b - Math.sqrt(d)) / (2 * a));
-	}
+	} else {
+    arr = [];
+  }
 
 	return arr;
 }
 //console.log(solveEquation(1, 2, -3));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let P = percent / 100 / 12;
+	let P = (percent / 100) / 12;
 	let S = amount - contribution;
-	let monthlyPayment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
-	let fullPay = (monthlyPayment * countMonths).toFixed(2);
-
-	return fullPay;
+	let monthlyPayment = S * (P + (P / (Math.pow((1 + P), countMonths) - 1)));
+	let fullPay = (monthlyPayment * countMonths);
+  
+	return (fullPay.toFixed(2));
 }
 //console.log(calculateTotalMortgage(10, 0, 50000, 12));
+// Math.pow(7, 2); // 49  (((1 + P)** countMonths) - 1) 
