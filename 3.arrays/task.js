541943918d0e22b13332c725a0af0a60409822f4
Dima Lis <iselis777888999@gmail.com>
2024-02-04 21:29:53 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (
+      arr1.length === arr2.length &&
+      arr1.every((item, index) => item === arr2[index])
+    ) {
+      return true;
+    }
+    return false;
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender){
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => acc + item / arr.length, 0);
+    return console.log (result);
+  }
