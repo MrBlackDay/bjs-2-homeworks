679a3c5ee985c49eda6685e7a9e6a17775164dd6
Dima Lis <iselis777888999@gmail.com>
2024-02-18 13:50:01 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿"use strict"
+
+function parseCount(count) {
+	let parsedCount = parseFloat(count);
+	if (isNaN(parsedCount)) {
+		throw new Error('Невалидное значение');
+	}
+	return parsedCount;
+}
+
+function validateCount(count) {
+	try {
+		return parseCount(count);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if ((a + b < c) || (a + c < b) || (c + b < a)) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	get perimeter() {
+		return (this.a + this.b + this.c);
+	}
+
+	get area() {
+		let semiP = this.perimeter / 2;
+		return (+Math.sqrt(semiP * (semiP - this.a) * (semiP - this.b) * (semiP - this.c)).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			}
+		}
+	}
+}
